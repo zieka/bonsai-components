72f0c0c46ccb236a672e6ae1d968ebc6aceee7c1
Kyle Scully <scullykns@gmail.com>
2022-10-28 20:31:38 -0700

    feat(utility-types): add partial and required picks

--- packages/utility-types/src/general-types.ts
+++ packages/utility-types/src/general-types.ts
@@ -1,1 +1,16 @@
+/**
+ * Construct a type with a set of properties K of type unknown
+ */
 export type UnknownRecord = Record<string | number | symbol, unknown>;
+
+/**
+ * Provide a Type (T) and pick which keys (K) should be made partials (allows undefined)
+ */
+export type PartialPick<T, K extends keyof T> = Partial<Pick<T, K>> &
+  Omit<T, K>;
+
+/**
+ * Provide a Type (T) and pick which keys (K) should be made required (not undefined)
+ */
+export type RequiredPick<T, K extends keyof T> = Required<Pick<T, K>> &
+  Omit<T, K>;
